42ae79154cbdde06910240db8d85448501b17371
root <root@DESKTOP-PAQNBSC.localdomain>
2023-04-11 12:33:45 +0700

    second

---
+++ pert4/nilai.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    cout<<"masukkan nilai : ";
+    cin>>i;
+
+    cout<<"nilai yang dimasukkan adalah "<<i<<endl;
+    cout<<"nilai jika digandakan "<<i*i<<endl;
+}
